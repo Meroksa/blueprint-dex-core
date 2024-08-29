d0fa20228e8934936ffdf52589cb6e93ef58e274
rends-east <rendseast@gmail.com>
2024-08-29 15:30:03 +0300

    swap script implemented

--- wrappers/Router.ts
+++ wrappers/Router.ts
@@ -44,83 +44,6 @@ export class Router implements Contract {
         });
     }
 
-
-
-    static swapMessage(jetton_amount: bigint, to: Address,
-        responseAddress: Address,
-        customPayload: Cell,
-        forward_ton_amount: bigint,
-        minOut: bigint,
-        to_address: Address,
-        has_ref: number,
-        ref_address: Address) {
-
-        let payload_cell = beginCell()
-                    .storeUint(0x25938561, 32)
-                    .storeCoins(minOut)
-                    .storeAddress(to_address)
-                    .storeBit(has_ref);
-        if (has_ref) {
-            payload_cell = payload_cell.storeAddress(ref_address);
-        }
-        return beginCell().storeUint(0xf8a7ea5, 32).storeUint(0, 64) // op, queryId
-            .storeCoins(jetton_amount).storeAddress(to)
-            .storeAddress(responseAddress)
-            .storeMaybeRef(customPayload)
-            .storeCoins(forward_ton_amount)
-            .storeMaybeRef(payload_cell.endCell())
-            .endCell();
-    }
-
-    async sendSwap(provider: ContractProvider, via: Sender,
-        value: bigint,
-        jetton_amount: bigint, to: Address,
-        responseAddress: Address,
-        customPayload: Cell,
-        forward_ton_amount: bigint,
-        minOut: bigint,
-        to_address: Address,
-        has_ref: number,
-        ref_address: Address) {
-        await provider.internal(via, {
-            sendMode: SendMode.PAY_GAS_SEPARATELY,
-            body: Router.swapMessage(jetton_amount, to, responseAddress, customPayload, forward_ton_amount, minOut, to_address, has_ref, ref_address),
-            value: value
-        });
-    }
-
-    static provideLpMessage(jetton_amount: bigint, to: Address,
-        responseAddress: Address,
-        customPayload: Cell,
-        forward_ton_amount: bigint,
-        minLpOut: bigint) {
-
-        let payload_cell = beginCell()
-        .storeUint(0xfcf9e58f, 32)
-        .storeCoins(minLpOut)
-        .endCell();
-        return beginCell().storeUint(0xf8a7ea5, 32).storeUint(0, 64) // op, queryId
-            .storeCoins(jetton_amount).storeAddress(to)
-            .storeAddress(responseAddress)
-            .storeMaybeRef(customPayload)
-            .storeCoins(forward_ton_amount)
-            .storeMaybeRef(payload_cell)
-            .endCell();
-    }
-
-    async provideLp(provider: ContractProvider, via: Sender,
-        value: bigint,
-        jetton_amount: bigint, to: Address,
-        responseAddress: Address,
-        customPayload: Cell,
-        forward_ton_amount: bigint,
-        minLpOut: bigint) {
-        await provider.internal(via, {
-            sendMode: SendMode.PAY_GAS_SEPARATELY,
-            body: Router.provideLpMessage(jetton_amount, to, responseAddress, customPayload, forward_ton_amount, minLpOut),
-            value: value
-        });
-    }
     
     async getDexData(provider: ContractProvider) {
         let res = await provider.get('get_router_data', []);
@@ -139,5 +62,5 @@ export class Router implements Contract {
             lp_account_code
         };
     }
-
+    
 }
